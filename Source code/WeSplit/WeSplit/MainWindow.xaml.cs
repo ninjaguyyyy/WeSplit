1b56483886bdc8f2d6da465aca738b5a79bc961b
a3chi <ninjaguyyyy@gmail.com>
2020-11-25 22:14:18 +0700

    layout -> menu

--- Source code/WeSplit/WeSplit/MainWindow.xaml.cs
+++ Source code/WeSplit/WeSplit/MainWindow.xaml.cs
@@ -26,13 +26,43 @@ namespace WeSplit
             
         }
 
-        /// <summary>
-        /// test thoi
-        /// </summary>
-        public void Test(int id)
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
         {
-            dynamic instance = 1;
-            Console.WriteLine(instance.name);
+            if (menuToggleButton.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_adding.Visibility = Visibility.Collapsed;
+                tt_settings.Visibility = Visibility.Collapsed;
+                tt_info.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_adding.Visibility = Visibility.Visible;
+                tt_settings.Visibility = Visibility.Visible;
+                tt_info.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void menuToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            mainPanel.Opacity = 1;
+        }
+
+        private void menuToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            mainPanel.Opacity = 0.3;
+        }
+
+        private void mainPanel_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            menuToggleButton.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 
