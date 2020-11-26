2deae27e3493c471fda867137e30e5624c7a213f
a3chi <ninjaguyyyy@gmail.com>
2020-11-26 17:41:28 +0700

    layout for detail, home + some component detail

---
+++ Source code/WeSplit/WeSplit/Constants/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeSplit.Constants
+{
+    class Color
+    {
+        public static readonly string primary = "#e37b58";
+        public static readonly string secondary = "#2C9AFF";
+    }
+}
